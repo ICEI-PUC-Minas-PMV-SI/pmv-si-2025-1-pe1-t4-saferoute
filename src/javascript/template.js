d05dee32e043dc2696fb1c6cbbfe9fb081a4a603
Roberto Rodrigues Guimarãis <roberto_guimarais@hotmail.com>
2025-04-30 02:09:47 -0300

    Add files via upload

---
+++ src/javascript/template.js
@@ -0,0 +1,8 @@
+function exibir_opcoes () {
+    var aside  = document.getElementById('menu_opcoes');
+    if (aside.style.display==='none') {
+        aside.style.display='block';
+    } else {
+        aside.style.display='none'; 
+ }
+}
