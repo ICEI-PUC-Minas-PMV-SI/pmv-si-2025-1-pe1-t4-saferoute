1abe6c7ebdc42948062fa97d15b8fa2d2e4455bc
RobertoGuimarais <roberto_guimarais@hotmail.com>
2025-05-28 23:14:34 -0300

    Alterações na tela de cadastro

--- src/json_server/public/javascript/cadastro.js
+++ src/json_server/public/javascript/cadastro.js
@@ -16,7 +16,16 @@ function sair() {
 }
 
 
-function salvar(){   
+function salvar(){
+    const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;   
+    if (!email_regex.test(document.querySelector('#email').value)) {
+    Swal.fire({
+      icon: 'error',
+      text: 'Favor preencher um endereço de e-mail válido!',
+      confirmButtonText: 'OK'
+    });
+    return;
+  }
     if (document.querySelector('#email').value !== document.querySelector('#confirmar_email').value) {
       Swal.fire({
         icon: 'error',
@@ -45,28 +54,33 @@ function salvar(){
         confirmButtonText: 'OK'
         });
         return;
-      } else {    campos = ["email", "senha", "nome", "sobrenome","cep", "endereco", "numero", "complemento", 
+      } else { campos = ["email", "senha", "nome", "sobrenome","cep", "endereco", "numero", "complemento", 
               "bairro", "municipio", "uf"]
-    let usuario = {}
+    let usuario = {};
+    let campo_vazio = false;
     campos.forEach(campo => {
         if (document.querySelector('#'+campo).value.length===0){
-            let nome_campo = campo.charAt(0).toUpperCase() + campo.slice(1)
-               if (nome_campo==="Endereco"){
+          let nome_campo = campo.charAt(0).toUpperCase() + campo.slice(1)
+               if (nome_campo==="Endereco"){ 
+                campo_vazio = true;
                 Swal.fire({
                     icon: 'error',
                     text: 'Favor informar o "Endereço"!',
                     confirmButtonText: 'OK'
-                });} else if (nome_campo==="Numero"){
+                })} else if (nome_campo==="Numero"){
+                campo_vazio = true;
                 Swal.fire({
                     icon: 'error',
                     text: 'Favor informar o "Número"!',
                     confirmButtonText: 'OK'
                 });} else if (nome_campo==="Uf"){
-                                Swal.fire({
+                   campo_vazio = true; 
+                   Swal.fire({
                     icon: 'error',
                     text: 'Favor informar a "UF"!',
                     confirmButtonText: 'OK'
                 });} else if (nome_campo!=="Complemento"){
+                    campo_vazio = true;
                     Swal.fire({
                     icon: 'error',
                     text: `Favor informar o "${nome_campo}"!`,
@@ -79,6 +93,8 @@ function salvar(){
                 })
         usuario["sexo"] = document.querySelector('input[name="radio_sexo"]:checked').value
         usuario["imagem"] = usuario["nome"] + "_" + usuario["sobrenome"] + ".png"
+       console.log(campo_vazio)
+       if (campo_vazio===false){
         fetch('http://localhost:3000/usuarios', {
           method: 'POST',
           headers: {
@@ -94,7 +110,7 @@ function salvar(){
         }).then((result) => {
            if (result.isConfirmed) {
         window.location.href = "login.html"
-        }})});
+        }})});}
       }})  
       }
  
@@ -132,6 +148,34 @@ function alterar_imagem(){
   });
 }
  
+function buscar_cep (){
+  const cep = document.querySelector('#cep').value;
+  const url_cep = 'https://viacep.com.br/ws/'+ cep +'/json/';
+  fetch(url_cep)
+        .then(response => {if (!response.ok){
+          throw new Error('Erro na resposta da API');
+        } 
+        return response.json()})
+        .then(data => {if (data['cep']!==undefined){
+        document.querySelector('#endereco').value=data['logradouro']
+        document.querySelector('#bairro').value=data['bairro']
+        document.querySelector('#municipio').value=data['localidade']
+        document.querySelector('#uf').value=data['uf']
+        } else {
+            Swal.fire({
+            icon: 'error',
+            text: 'Não foi possível localizar o CEP pesquisado!',
+            confirmButtonText: 'OK'            
+        })}
+            })
+            .catch(error => {
+      Swal.fire({
+      icon: 'error',
+      text: 'Não foi possível localizar o CEP pesquisado!',
+      confirmButtonText: 'OK'
+    });})
+    }
+    
 window.onload = function() {
     masculino.checked = true;
 }
